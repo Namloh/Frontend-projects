ec305a4e22230cade38e863f38705d3a490fc1b5
Matěj Holman <matej.holman.020@pslib.cz>
2023-03-02 17:20:58 +0100

    done, hopefully

--- js.js
+++ js.js
@@ -5,3 +5,19 @@ $(function () {
         $navbar.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
       });
   });
+
+
+var arrows =  document.getElementsByClassName('arr-btn')
+var items = document.getElementsByClassName('carousel-item active')
+
+
+
+Array.from(arrows).forEach(function (element) {
+    element.addEventListener("click", function(){
+        if(items.length > 1){
+            items[0].classList.remove("active")
+        }
+    })
+});
+
+
