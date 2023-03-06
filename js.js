8a095b0363a307f0f6d8d96b75e5a26a29b22e34
Matěj Holman <matej.holman.020@pslib.cz>
2023-03-06 19:19:02 +0100

    pupupu

--- js.js
+++ js.js
@@ -7,10 +7,30 @@ $(function () {
   });
 
 
+var hamburger =  document.getElementById('hamburger')
+var navbar =  document.getElementsByClassName('navbar')
+var collapse =  document.getElementsByClassName('navbar-collapse')
+var opened = false;
+
+hamburger.addEventListener("click", function(){
+    console.log(opened)
+    if(opened){
+        setTimeout(() => {
+            navbar[0].classList.remove("scrolled")
+          }, "330");
+        opened = false;
+    }
+    else{
+        navbar[0].classList.add("scrolled")
+        opened = true;
+    }
+    
+       
+})
+
+/*
 var arrows =  document.getElementsByClassName('arr-btn')
 var items = document.getElementsByClassName('carousel-item active')
-
-
 
 Array.from(arrows).forEach(function (element) {
     element.addEventListener("click", function(){
@@ -19,5 +39,5 @@ Array.from(arrows).forEach(function (element) {
         }
     })
 });
-
+*/
 
